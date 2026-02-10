08b2b259946c61e94f36f8c95736805d7edebccc
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-02-10 23:04:27 +0530

    controller folder created

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,59 +1,8 @@
 const express = require('express');
-const User = require('../models/user.model');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+const { signUp } = require('../controllers/auth.controllers');
 
 const router = express.Router();
 
-router.post('/sign-up', async (req, res) => {
-
-    const { email, password, username, bio, profilePicture } = req.body;
-
-    const isExists = await User.findOne({
-        $or: [
-            { email },
-            { username }
-        ]
-    });
-
-    if (isExists) {
-        return res.status(400).json({
-            message: isExists.email === email && isExists.username === username ? "email and username are already exists."
-                : isExists.email === email ? "email already exists."
-                    : "username already exists."
-        });
-    };
-
-    const user = await User.create({
-        email,
-        password: crypto.createHash('sha256').update(password).digest('hex'),
-        username,
-        bio,
-        profilePicture
-    });
-
-    const payload = {
-        id: user._id
-    }
-
-    const token = jwt.sign(
-        payload,
-        process.env.JWT_SECRET,
-        { expiresIn: '1d' }
-    );
-
-    res.cookie('jwt_token', token);
-
-    res.status(201).json({
-        message: "User registered successfully.",
-        user: {
-            email: user.email,
-            username: user.username,
-            bio: user.bio,
-            profilePicture: user.profilePicture
-        },
-        token
-    });
-});
+router.post('/sign-up', signUp);
 
 module.exports = router;
