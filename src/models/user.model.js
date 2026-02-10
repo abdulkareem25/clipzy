c3261dce958a4662416fd34cba79c1e88faea7e9
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-02-10 23:00:34 +0530

    proper register api created

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://ik.imagekit.io/abdulkareem25/default_pfp.jpg"
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
