f4da03555d3cc8217aae0d62e4b2b7e2341e09e5
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-02-11 23:44:29 +0530

    sign in controller under construction

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,4 +5,10 @@ const router = express.Router();
 
 router.post('/sign-up', signUp);
 
+router.post('/sign-in', (req, res) => {
+    res.status(200).json({
+        message: "Sign in route is under construction."
+    });
+});
+
 module.exports = router;
