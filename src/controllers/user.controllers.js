7229d1d62a1025d4f8368903472214c1bb640c26
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-02-20 09:48:01 +0530

    unfollow feature created

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,7 +2,7 @@ const Follow = require("../models/follow.model");
 const User = require("../models/user.model");
 
 async function followUser(req, res) {
-    
+
     const { followeeId } = req.params;
     const followerId = req.user.userId;
 
@@ -47,6 +47,35 @@ async function unFollowUser(req, res) {
     const { followeeId } = req.params;
     const followerId = req.user.userId;
 
+    const isFolloweeExist = await User.findById(followeeId);
+
+    if(!isFolloweeExist) {
+        return res.status(400).json({
+            message: "User you are trying to unfollow does not exist."
+        });
+    };
+
+    if (followerId === followeeId) {
+        return res.status(400).json({
+            message: "You cannot unfollow yourself"
+        });
+    };
+
+    const isFollowing = await Follow.findOne({ 
+        followeeId,
+        followerId
+    });
+
+    if(isFollowing) {
+        await Follow.findOneAndDelete({
+            followeeId,
+            followerId
+        });
+    };
+
+    res.status(200).json({
+        message: "User unfollowed successfully."
+    });
 };
 
-module.exports = { followUser };
+module.exports = { followUser, unFollowUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const identifyUser = require('../middlewares/auth.middleware');
-const { followUser } = require('../controllers/user.controllers');
+const { followUser, unFollowUser } = require('../controllers/user.controllers');
 const validateObjectId = require('../middlewares/validateObjectId.middleware');
 
 const router = express.Router();
 
-router.post('/follow/:followeeId', validateObjectId('followeeId'), identifyUser, followUser);
+router.post(
+    '/follow/:followeeId',
+    validateObjectId('followeeId'),
+    identifyUser,
+    followUser
+);
+
+router.post(
+    '/unfollow/:followeeId',
+    validateObjectId('followeeId'),
+    identifyUser,
+    unFollowUser
+);
 
 module.exports = router;
