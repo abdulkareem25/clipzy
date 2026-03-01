6ef4bb3ce00f477e99eae3c9c602663a99e246fe
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-03-01 23:44:09 +0530

    authcontext created

---
+++ Frontend/src/features/auth/services/auth.context.js
@@ -0,0 +1,71 @@
+import { createContext, useState } from "react";
+import { signUp, signIn, signOut, getUser } from "./auth.api";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleSignUp = async () => {
+        setLoading(true);
+        try {
+            const response = await signUp(username, email, password);
+            setUser(response.user);
+        } catch (error) {
+            console.error("handleSignUp error:", error);
+        } finally {
+            setLoading(false);
+        };
+    };
+
+    const handleSignIn = async () => {
+        setLoading(true);
+        try {
+            const response = await signIn(email, password);
+            setUser(response.user);
+        } catch (error) {
+            console.error("handleSignIn error:", error);
+        } finally {
+            setLoading(false);
+        };
+    };
+
+    const handleSignOut = async () => {
+        setLoading(true);
+        try {
+            await signOut();
+            setUser(null);
+        } catch (error) {
+            console.error("handleSignOut error:", error);
+        } finally {
+            setLoading(false);
+        };
+    };
+
+    const fetchUser = async () => {
+        setLoading(true);
+        try {
+            const response = await getUser();
+            setUser(response.user);
+        } catch (error) {
+            console.error("fetchUser error:", error);
+        } finally {     
+            setLoading(false);
+        };
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            user,
+            loading, 
+            handleSignUp, 
+            handleSignIn,
+            handleSignOut,
+            fetchUser
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
