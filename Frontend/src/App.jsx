59be5afbd509c794b1a746a6e9ffc0255fb7bb6c
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-03-01 23:49:47 +0530

    approutes file created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router"
-import SignUp from "./features/auth/pages/SignUp"
-import SignIn from "./features/auth/pages/SignIn"
+import AppRoutes from './AppRoutes'
+import { AuthProvider } from './features/auth/services/auth.context'
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   )
-}
+};
 
 export default App
---
+++ Frontend/src/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router"
+import SignUp from "./features/auth/pages/SignUp"
+import SignIn from "./features/auth/pages/SignIn"
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="*" element={<h1>404 Not Found</h1>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
